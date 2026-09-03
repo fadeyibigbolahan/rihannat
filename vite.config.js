d16b93664b94970d9e3f9cef561ad354fe8fd7ab
fadeyibi gbolahan <fadeyibi26@gmail.com>
2026-09-03 23:21:28 +0100

    blank

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/rihannat/", // Add this line (your repo name)
+  base: "/", // Add this line (your repo name)
 });
